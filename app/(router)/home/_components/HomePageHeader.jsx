bc86b137aced3fa80cf55d2379525a4d2a71d5df
g.m.sakujashamalgajanayake <sakuja1shamal@gmail.com>
2024-03-26 15:41:12 +0530

    Update 2.2

--- app/(router)/home/_components/HomePageHeader.jsx
+++ app/(router)/home/_components/HomePageHeader.jsx
@@ -71,13 +71,16 @@ function HomePageHeader() {
             <header ref={headerRef} className="z-50 sticky top-0 flex h-16 items-center gap-4 bg-background px-4 md:px-6">
                 <nav ref={menuRef} onClick={toggleMenu}
                     className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-                    <Link onClick={()=>{router.push('/home')}}
-                        href="#"
-                        className="w-[80px] h-auto flex items-center justify-center gap-2 text-lg font-semibold md:text-base"
+                    <Link onClick={() => {
+                        router.push('/home')
+                    }}
+                          href="#"
+                          className="w-[80px] h-auto flex items-center justify-center gap-2 text-lg font-semibold md:text-base"
                     >
-                        <Image onClick={()=>router.push('/home')} className={'cursor-pointer'} src={'/BMS-logo.png'}
-                               width={100} height={80} alt="logo"/>
-                        <span className="sr-only">Acme Inc</span>
+
+                        <h1 className="text-headingColor font-[800] text-[1.8rem] sm:text-[40px] leading-[35px] sm:leading-[40px] mt-5 mb-5">
+                            BMS
+                        </h1>
                     </Link>
                     <Link
                         href="#contact"
@@ -132,9 +135,9 @@ function HomePageHeader() {
                                 href="#"
                                 className="flex items-center gap-2 text-lg font-semibold"
                             >
-                                <Image src={'/BMS-logo.png'}
-                                       width={60} height={40} alt="logo"/>
-                                <span className="sr-only">Acme Inc</span>
+                                <h1 className="text-headingColor font-[800] text-[1.8rem] sm:text-[40px] leading-[35px] sm:leading-[40px] mt-5 mb-5">
+                                    BMS
+                                </h1>
                             </Link>
                             <Link href="#" className="hover:text-foreground">
                                 Home
